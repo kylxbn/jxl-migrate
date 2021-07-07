d8f3cbd3acf4c5a6d1f1cff765db7ec3c09deb31
Kyle Alexander Buan <me@kylxbn.com>
2021-07-07 11:49:33 +0800

    Add license to source

--- migrate.py
+++ migrate.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+
+'''
+jxl-migrate - Convert images to JPEG XL (JXL) format
+Copyright (C) 2021-present Kyle Alexander Buan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 import os
 import subprocess
 from subprocess import check_output
